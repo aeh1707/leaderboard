ff6069b381a2ee9f51d010fe605886ecb8204363
Abdelhadi Hireche <abdelhadi.hireche@outlook.com>
2022-06-30 12:42:31 +0000

    Revert "Update 7 files"
    This reverts commit 537f0b6d52f04cbd10c208c2bd07ebed418bcc57.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import _ from 'lodash';
 import './style.css';
 
-class UserScore {
-  constructor(username, score) {
-    this.username = username;
-    this.score = score;
-  }
+function component() {
+	const element = document.createElement('div');
+
+	// Lodash, now imported by this script
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	return element;
 }
 
-let userScoresList = JSON.parse(localStorage.getItem('userScoresList')) || [];
-const submit = document.querySelector('form button');
-const usernameInput = document.querySelector('#username');
-const scoreInput = document.querySelector('#score');
-
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  let newUserScore = new UserScore(usernameInput.value, scoreInput.value);
-  usernameInput.value = '';
-  scoreInput.value = '';
-  userScoresList.push(newUserScore);
-  localStorage.setItem('userScoresList', JSON.stringify(userScoresList));
-  console.log(userScoresList);
-});
+document.body.appendChild(component());
